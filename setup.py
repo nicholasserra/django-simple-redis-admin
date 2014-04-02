5310ddc7493412788d45c26815fd6b553d5279e1
Nicholas Serra <nicks@ydekproductions.com>
2014-04-02 19:05:49 -0400

    Bump version and changelog

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-simple-redis-admin',
-    version='1.0.2',
+    version='1.1.0',
     description='A django admin application to manage redis cache keys.',
     long_description=open('README.md').read(),
     author='Nicholas Serra',
