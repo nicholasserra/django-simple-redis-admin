a35b4470c384a5409cd3742af0b1fd942ed6210a
Mark Beacom <Beacom@gmail.com>
2016-09-05 23:19:00 -0400

    Remove usage of deprecated patterns for Django 1.10.

--- redis_admin/admin.py
+++ redis_admin/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.db import models
-from django.conf.urls import patterns
+from django.conf.urls import url
 from django.core.cache import cache
 from django.template import RequestContext
 from django.shortcuts import render_to_response
@@ -16,11 +16,11 @@ class RedisAdmin(admin.ModelAdmin):
     def get_urls(self):
         urls = super(RedisAdmin, self).get_urls()
 
-        my_urls = patterns('',
-            (r'^$', self.index),
-            (r'^(?P<key>.+)/delete/$', self.delete),
-            (r'^(?P<key>.+)/$', self.key),
-        )
+        my_urls = [
+            url(r'^$', self.index),
+            url(r'^(?P<key>.+)/delete/$', self.delete),
+            url(r'^(?P<key>.+)/$', self.key),
+        ]
         return my_urls + urls
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
@@ -31,18 +31,18 @@ class RedisAdmin(admin.ModelAdmin):
             request.POST.get('post') == 'yes':
 
             if cache.master_client.delete(*request.POST.getlist('_selected_action')):
-                messages.add_message(request, messages.SUCCESS, 
+                messages.add_message(request, messages.SUCCESS,
                                     'Successfully deleted %d keys.' %
                                     len(request.POST.getlist('_selected_action')))
             else:
-                messages.add_message(request, messages.ERROR, 
+                messages.add_message(request, messages.ERROR,
                                     'Could not delete %d keys.' %
                                     len(request.POST.getlist('_selected_action')))
 
         elif request.method == 'POST' and request.POST.getlist('_selected_action') and \
             request.POST.get('action') == 'delete_selected':
- 
-            return render_to_response('redis_admin/delete_selected_confirmation.html', 
+
+            return render_to_response('redis_admin/delete_selected_confirmation.html',
                                      {'keys': request.POST.getlist('_selected_action')},
                                      context_instance=RequestContext(request))
 
@@ -81,7 +81,7 @@ class RedisAdmin(admin.ModelAdmin):
         elif key_type == 'set':
             context['value'] = str(cache.master_client.smembers(key))
 
-        return render_to_response('redis_admin/key.html', context, 
+        return render_to_response('redis_admin/key.html', context,
                                    context_instance=RequestContext(request))
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
@@ -92,7 +92,7 @@ class RedisAdmin(admin.ModelAdmin):
             else:
                 messages.add_message(request, messages.ERROR, 'The key "%s" was not deleted successfully.' % key)
             return HttpResponseRedirect('%sredis_admin/manage/' % reverse('admin:index'))
-        return render_to_response('redis_admin/delete_confirmation.html', 
+        return render_to_response('redis_admin/delete_confirmation.html',
                                  {'key': key}, context_instance=RequestContext(request))
 
 class Meta:
